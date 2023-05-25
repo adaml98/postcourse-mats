09e0dc3317f63b277e970f34bf538e8c428d04f4
adaml98 <47745325+adaml98@users.noreply.github.com>
2023-05-25 16:43:09 +0100

    complete first 2 challenges

--- back_end_tech_tests/1. Intermediate/challenge_1.1/toggleDoors.js
+++ back_end_tech_tests/1. Intermediate/challenge_1.1/toggleDoors.js
@@ -1,3 +1,22 @@
-const toggleDoors = () => {}
+const toggleDoors = (n) => {
+  const doorArray = Array(n).fill(0);
+  let increase = 1;
+  for (let i = 0; i < doorArray.length; i++) {
+    for (let j = i; j < doorArray.length; j += increase) {
+      if (doorArray[j] === 0) {
+        doorArray[j] = 1;
+      } else if (doorArray[j] === 1) {
+        doorArray[j] = 0;
+      }
+    }
+    increase++;
+  }
+
+  const resultArray = [];
+  for (let i = 0; i < doorArray.length; i++) {
+    if (doorArray[i] === 1) resultArray.push(i + 1);
+  }
+  return resultArray;
+};
 
 module.exports = toggleDoors;
--- back_end_tech_tests/1. Intermediate/challenge_1.2/isCoPrime.js
+++ back_end_tech_tests/1. Intermediate/challenge_1.2/isCoPrime.js
@@ -1,3 +1,11 @@
-const isCoPrime = (num1, num2) => {}
+const isCoPrime = (num1, num2) => {
+  if (num1 === num2) return false;
+  for (let i = 2; i < num1 || i < num2; i++) {
+    if (num1 % i === 0 && num2 % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
 
 module.exports = isCoPrime;
