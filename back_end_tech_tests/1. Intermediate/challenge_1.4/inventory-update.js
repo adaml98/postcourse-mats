0d0e70aa3527ba8fa49869710c792461ece86c40
adaml98 <47745325+adaml98@users.noreply.github.com>
2023-06-02 12:44:43 +0100

    complete first test

--- back_end_tech_tests/1. Intermediate/challenge_1.4/inventory-update.js
+++ back_end_tech_tests/1. Intermediate/challenge_1.4/inventory-update.js
@@ -1,3 +1,15 @@
-function updateInventory() {}
+function updateInventory(arr1, arr2) {
+  const answer = [];
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i][1] === arr2[j][1]) {
+        answer.push([arr1[i][0] + arr2[j][0], arr1[i][1]]);
+      }
+    }
+    answer.push(arr1[i]);
+  }
+  console.log(answer);
+  return answer;
+}
 
 module.exports = updateInventory;
