011cf150b4ff90a24d7081b2d4a78c1b71b9e344
adaml98 <47745325+adaml98@users.noreply.github.com>
2023-06-02 12:11:11 +0100

    complete challenge 1.3

--- back_end_tech_tests/1. Intermediate/challenge_1.3/diffToFib.js
+++ back_end_tech_tests/1. Intermediate/challenge_1.3/diffToFib.js
@@ -1,6 +1,10 @@
 function diffToFib(num) {
   const fib = [0, 1];
-  for (let i = 0; i < num; i++) {}
+  for (let i = 1; fib[i] <= num; i++) {
+    if (num === fib[i]) return 0;
+    fib.push(fib[i] + fib[i - 1]);
+  }
+  return fib[fib.length - 1] - num;
 }
 
 module.exports = diffToFib;
