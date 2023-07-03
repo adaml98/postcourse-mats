9639191ed4f4d1f4e844cb08f466bc4ea072aa57
adaml98 <47745325+adaml98@users.noreply.github.com>
2023-07-03 11:37:53 +0100

    complete 2.1

--- back_end_tech_tests/2. String Manipulation/challenge_2.1/reverseCapitalisation.js
+++ back_end_tech_tests/2. String Manipulation/challenge_2.1/reverseCapitalisation.js
@@ -1,3 +1,12 @@
-const reverseCapitalisation = () => {}
+const reverseCapitalisation = (word) => {
+  return word
+    .split("")
+    .map((letter) => {
+      return letter === letter.toLowerCase()
+        ? letter.toUpperCase()
+        : letter.toLowerCase();
+    })
+    .join("");
+};
 
 module.exports = reverseCapitalisation;
